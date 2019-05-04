7e207580b95efd1ccff78dccef510f6ddebda921
Isabella Tea <tea.isabella@gmail.com>
2019-05-04 03:10:17 -0700

    starter files

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+//starter function to pull a random pokemon from the PokeAPI
+  function getRandomPokemonLeft() {
+    let randomNumber = Math.floor(Math.random() * 800) + 1;
+    $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
+      console.log("Left Pokemon Data: ", pokeData);
+    });
+  }
+
+
+
+//all code here
+
+
+
+
+
+})
